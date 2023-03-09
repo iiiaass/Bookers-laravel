c27e405693d976080e3b8b2f143ca7436271fa44
iiiaass <asamikannazaki@gmail.com>
2023-03-09 18:01:53 +0900

    storeメソッド完了

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Memo;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function create()
+    {
+        //ログインしているユーザーの情報をViewに渡す
+        $user = \Auth::user();
+        return view('create',compact('user'));
+    }
+
+    public function store(Request $request) //Requestを使うと、フォームに入力されたメモの内容、ユーザーidをコントローラで受け取ることが出来る。//
+    {
+        $data = $request->all();  //$dataの中に$request allでHTMLから投げられたデータを全て$dataに入れている
+        //dd($data);    //dd->その$dataの中身を分解して画面で確認できる
+        // POSTされたデータをDB（memosテーブル）に挿入
+        // MEMOモデルにDBへ保存する命令を出す
+
+        $memo_id = Memo::insertGetId([   //insert それぞれデータを定義して、データベースに挿入していっている
+            'content' => $data['content'],
+             'user_id' => $data['user_id'], 
+             'status' => 1
+        ]); 
+        
+        // リダイレクト処理->別のページへ遷移すること
+        return redirect()->route('home');
+    }
+    
+}   
+
+
