fe3546023b0978f49d413660dc886c6f1c935b8d
iiiaass <asamikannazaki@gmail.com>
2023-03-12 19:26:19 +0900

    コミット遅れた者たち

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Memo;
 use App\Models\Tag;
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
@@ -84,9 +85,9 @@ class HomeController extends Controller
     public function update(Request $request, $id)    //$id->editのURLパラメータと同じく$idと書くことによってどこの行を更新するか受け取ることができる
     {
         $inputs = $request->all();
-        dd($inputs);
-        // Memo::where('id', $id)->update(['content' => $inputs['content'],'tag_id'=>$inputs['tag_id']]);     //whereでどこをupdateするのか指定する。idはURLパラメータに入っているもの、そのあとupdateしたい内容を配列で指定。contentの内容を更新したいので$inputsのcontentの内容に更新します。という意味
-        // return redirect()->route('home');    // リダイレクト処理->別のページへ遷移すること                                                                        
+        // dd($inputs);
+        Memo::where('id', $id)->update(['content' => $inputs['content'],'tag_id'=>$inputs['tag_id']]);     //whereでどこをupdateするのか指定する。idはURLパラメータに入っているもの、そのあとupdateしたい内容を配列で指定。contentの内容を更新したいので$inputsのcontentの内容に更新します。という意味
+        return redirect()->route('home');    // リダイレクト処理->別のページへ遷移すること                                                                        
     }
 }   
 
